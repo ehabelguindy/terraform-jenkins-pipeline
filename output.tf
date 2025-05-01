873344e719fdf46f119c08ba0ea9801d6a5238ed
ehabelguindy <eelguindy@gmail.com>
2025-05-01 15:20:13 +0300

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,12 +1,14 @@
 output "public_ip" {
- value       = aws_instance.public_instance.public_ip
- description = "Public IP Address of EC2 instance"
+  value       = aws_instance.public_instance.public_ip
+  description = "Public IP Address of EC2 instance"
 }
 
 output "instance_id" {
- value       = aws_instance.public_instance.id
- description = "Instance ID"
+  value       = aws_instance.public_instance.id
+  description = "Instance ID"
 }
+
 output "ec2_public_ip" {
-  value = aws_instance.my_ec2_instance.public_ip
+  value       = aws_instance.public_instance.public_ip
+  description = "Public IP Address (alias output)"
 }
